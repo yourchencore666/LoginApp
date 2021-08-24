cca627000e9228b6973b39bcee59cb446303365c
Artem Yurchenko <yourchencore@yandex.ru>
2021-08-24 09:37:52 +0200

    create interface and outlets

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func clickLogOutBtn() {
+    }
 }
-
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by user202124 on 24.08.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func clickLogInBtn() {
+    }
+    
+    @IBAction func clickForgotUserNameBtn() {
+    }
+    
+    @IBAction func clickForgotPasswordBtn() {
+    }
+}
